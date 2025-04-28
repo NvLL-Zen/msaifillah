b078b90ed4775d206d8941c13374f393d1d82463
NvLL-Zen <nvll.zen@gmail.com>
2025-04-28 21:28:28 +0700

    V1, added scaffolding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import "../styles/global.css"
+    import "../styles/navbar.css"
+    import "../styles/section.css"
+</script>
+
+<nav class="navbar">
+    <a class="navTitle" href="./">SAIFILLAH</a>
+    <ul>
+        <li>Tentang</li>
+        <li>Berita</li>
+        <li>Kegiatan</li>
+        <li>Kontak</li>
+    </ul>
+</nav>
+<section class="section" id="sectionSatu">
+    <h1>Foto masjid saifillah pagi hari + malem hari (tergantung waktu WIB, jadi nanti klo malem malem, klo pagi2, dua foto aja)</h1>
+</section>
+<section class="section" id="sectionDua">
+    <h1>Tentang masjid saifillah</h1>
+</section>
+<section class="section" id="sectionTiga">
+    <h1>Informasi2 terkait (kegiatan, program, dan lain2)</h1>
+</section>
+<section class="section" id="sectionEmpat">
+    <h1>Kontak (sm QRIS buat infaq klo mau, tinggal ss trs taruh sini)</h1>
+</section>
+<section class="section" id="footer">
+    <h1>Copyright Masjid Saifillah 2025 | <a target="_blank" href="https://zenriel.tech">By Fauzan Rizky N</a></h1>
+</section>
